9a67c082609ffd0f31edb1c04c47ab07690465a4
Amit binu <binua@mcmaster.ca>
2017-07-10 09:36:15 -0400

    Added methods to get user's input

--- app/src/main/java/com/example/android/calculator/MainActivity.java
+++ app/src/main/java/com/example/android/calculator/MainActivity.java
@@ -10,4 +10,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    public static int clicked1(){
+        return 1;
+    }
+    public static int clicked2(){
+        return 2;
+    }
+    public static int clicked3(){
+        return 3;
+    }
+    public static int clicked4(){
+        return 4;
+    }
+    public static int clicked5(){
+        return 5;
+    }
+    public static int clicked6(){
+        return 6;
+    }
+    public static int clicked7(){
+        return 7;
+    }
+    public static int clicked8(){
+        return 8;
+    }
+    public static int clicked9(){
+        return 9;
+    }
+
+
 }
