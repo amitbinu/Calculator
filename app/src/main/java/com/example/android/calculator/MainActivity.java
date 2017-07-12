9d3f79be4ec9e236d6b44861f7341f672fc80911
Amit binu <binua@mcmaster.ca>
2017-07-12 13:43:40 -0400

    Modified to show user what the user is typing

--- app/src/main/java/com/example/android/calculator/MainActivity.java
+++ app/src/main/java/com/example/android/calculator/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.android.calculator;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,35 +15,55 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.textView);
     }
 
+    ArrayList<String> userInput = new ArrayList<>();
+    private static String number = "";
+    private static String operator = "";
+    private static TextView textView;
 
-    public static int clicked1(){
-        return 1;
+    public static void clicked1(View view){
+        number += "1";
+        updateInput();
     }
-    public static int clicked2(){
-        return 2;
+    public static void clicked2(View view){
+        number += "2";
+        updateInput();
     }
-    public static int clicked3(){
-        return 3;
+    public static void clicked3(View view){
+        Log.e("Number",view.getId()+"");
+        number += "3";
+        updateInput();
     }
-    public static int clicked4(){
-        return 4;
+    public static void clicked4(View view){
+        number += "4";
+        updateInput();
     }
-    public static int clicked5(){
-        return 5;
+    public static void clicked5(View view){
+        number += "5";
+        updateInput();
     }
-    public static int clicked6(){
-        return 6;
+    public static void clicked6(View view){
+        number += "6";
+        updateInput();
     }
-    public static int clicked7(){
-        return 7;
+    public static void clicked7(View view){
+        number += "7";
+        updateInput();
     }
-    public static int clicked8(){
-        return 8;
+    public static void clicked8(View view){
+        number += "8";
+        updateInput();
+
     }
-    public static int clicked9(){
-        return 9;
+    public static void clicked9(View view){
+        number += "9";
+        updateInput();
+    }
+
+    public static void updateInput(){
+        textView.setText(number);
     }
 
 
