6c97a9be23114409c607b2d52745f07c61376421
Amit binu <binua@mcmaster.ca>
2017-07-13 20:21:49 -0400

    Modified to do addition and multiplication properly

--- app/src/main/java/com/example/android/calculator/MainActivity.java
+++ app/src/main/java/com/example/android/calculator/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.android.calculator;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,6 +7,21 @@ import android.view.View;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+
+/*
+*<TextView
+            android:layout_width="match_parent"
+            android:layout_height="90dp"
+            android:id="@+id/textView"
+            android:text=""
+            android:textSize="80dp"
+            android:background="#e0e0e0"
+            android:fadingEdge="horizontal"
+            android:textColor="#000000"
+            android:scrollHorizontally="true"
+            android:maxLines="1"
+            android:scrollbars="none"/>
+* */
 
 public class MainActivity extends AppCompatActivity {
     private static ArrayList<String> userInput = new ArrayList<>();
@@ -21,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView = (TextView) findViewById(R.id.textView);
+        textView.setHorizontallyScrolling(true);
         answer = (TextView) findViewById(R.id.answer);
     }
 
@@ -63,8 +78,9 @@ public class MainActivity extends AppCompatActivity {
     public static void clicked7(View view){
         number += "7";
         showUser+="7";
+        Log.e("Clicked", "7");
         updateInput();
-        calculate();
+        //calculate();
     }
     public static void clicked8(View view){
         number += "8";
@@ -86,7 +102,7 @@ public class MainActivity extends AppCompatActivity {
         updateInput();
     }
     public static void clickedPlus(View view){
-        if(! (userInput.size() < 1)){
+        if(number != "" && number != "."){
             showUser += "+";
             userInput.add(number);
             userInput.add("+");
@@ -96,7 +112,7 @@ public class MainActivity extends AppCompatActivity {
 
         }
     public static void clickedMinus(View view){
-        if(! (userInput.size() < 1)){}
+        if(number != "" && number != "."){}
         showUser += "-";
         userInput.add(number);
         userInput.add("-");
@@ -104,7 +120,7 @@ public class MainActivity extends AppCompatActivity {
         updateInput();
     }
     public static void clickedMult(View view){
-        if(! (userInput.size() < 1)){}
+        if(number != "" && number != "."){}
         showUser += "x";
         userInput.add(number);
         userInput.add("*");
@@ -112,7 +128,7 @@ public class MainActivity extends AppCompatActivity {
         updateInput();
     }
     public static void clickedDiv(View view){
-        if(! (userInput.size() < 1)){}
+        if(number != "" && number != "."){}
         showUser += "\u00F7";
         userInput.add(number);
         userInput.add("/");
@@ -128,23 +144,60 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public static void calculate(){
+        Log.e("IN", "calculate()");
         int lastIndex = userInput.size()-1;
-        boolean isOperator = false;
+        boolean isOperator;
+        ArrayList<Double> numbers = new ArrayList<>();
+        ArrayList<String> operators = new ArrayList<>();
         try{
         isOperator = userInput.get(lastIndex) == "+" || userInput.get(lastIndex) == "-" ||userInput.get(lastIndex) == "*" ||userInput.get(lastIndex) == "/";}
         catch (Exception e){
-
+            isOperator= false;
+            Log.e("Stuck", "141");
         }
         if(number!= "" && isOperator){
+
             for (int i =0 ; i < userInput.size(); i++){
                 try{
-                  Double number =   Double.parseDouble(userInput.get(i));
+                  Double number1 =   Double.parseDouble(userInput.get(i));
+                    numbers.add(number1);
                 }
                 catch (Exception e){
-
+                    operators.add(userInput.get(i));
                 }
             }
+            Double number1 = Double.parseDouble(number);
+            numbers.add(number1);
+
+            while (operators.size() >= 1){
+                Log.e("While loop", operators.size()+"");
+                while(operators.contains("*")){
+                    int index = operators.indexOf("*");
+                    double firstNumber = numbers.get(index);
+                    double secondNumber = numbers.get(index+1);
+                    operators.remove(index);
+                    numbers.remove(index+1);
+                    numbers.remove(index);
+                    double newNumber = firstNumber * secondNumber;
+                    numbers.add(index,newNumber);
+                    Log.e("While loop", "165");
+                }
+                while (operators.contains("+")){
+                    int index = operators.indexOf("+");
+                    double firstNumber = numbers.get(index);
+                    double secondNumber = numbers.get(index+1);
+                    operators.remove(index);
+                    numbers.remove(index+1);
+                    numbers.remove(index);
+                    double newNumber = firstNumber + secondNumber;
+                    numbers.add(index,newNumber);
+                    Log.e("While loop", "165");
+                }
+            }
+            answer.setText(numbers.get(0)+"");
         }
+
+
     }
 
 }
